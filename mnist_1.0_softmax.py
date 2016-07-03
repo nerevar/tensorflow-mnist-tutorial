34264bf8ac6149f95efe25840ac516c1fb86e1c0
Roman Rybalchenko (nerevar) <nerevar@yandex-team.ru>
2016-07-03 19:12:54 +0300

    add changing learning_rate and 3 NN levels

--- mnist_1.0_softmax.py
+++ mnist_1.0_softmax.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import math
 import tensorflow as tf
 import mnist_data
 import tensorflowvisu
@@ -38,21 +39,40 @@ tf.set_random_seed(0)
 # Download images and labels
 mnist = mnist_data.read_data_sets("data")
 
+neurons_1 = 10
+
 # input X: 28x28 grayscale images, the first dimension (None) will index the images in the mini-batch
 X = tf.placeholder(tf.float32, [None, 28, 28, 1])
 # correct answers will go here
-Y_ = tf.placeholder(tf.float32, [None, 10])
+Y_ = tf.placeholder(tf.float32, [None, neurons_1])
+
+
+neurons_3 = 200
+W3 = tf.Variable(tf.truncated_normal([784, neurons_3], stddev=0.1))  # 784 = 28 * 28
+B3 = tf.Variable(tf.ones([neurons_3])/10)
+
+neurons_2 = 110
+W2 = tf.Variable(tf.truncated_normal([neurons_3, neurons_2], stddev=0.1))  # 784 = 28 * 28
+B2 = tf.Variable(tf.ones([neurons_2])/10)
+
+
 # weights W[784, 10]   784=28*28
-W = tf.Variable(tf.zeros([784, 10]))
+W = tf.Variable(tf.zeros([neurons_2, neurons_1]))
 # biases b[10]
-b = tf.Variable(tf.zeros([10]))
+b = tf.Variable(tf.zeros([neurons_1]))
+
+
 
 # flatten the images into a single line of pixels
 # -1 in the shape definition means "the only possible dimension that will preserve the number of elements"
 XX = tf.reshape(X, [-1, 784])
 
 # The model
-Y = tf.nn.softmax(tf.matmul(XX, W) + b)
+
+Y3 = tf.nn.relu(tf.matmul(XX, W3) + B3)
+Y2 = tf.nn.relu(tf.matmul(Y3, W2) + B2)
+Y1 = tf.nn.softmax(tf.matmul(Y2, W) + b)
+Y = Y1
 
 # loss function: cross-entropy = - sum( Y_i * log(Yi) )
 #                           Y: the computed output vector
@@ -70,7 +90,11 @@ correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # training, learning rate = 0.005
-train_step = tf.train.GradientDescentOptimizer(0.005).minimize(cross_entropy)
+# train_step = tf.train.GradientDescentOptimizer(0.005).minimize(cross_entropy)
+# train_step = tf.train.GradientDescentOptimizer(0.003).minimize(cross_entropy)
+learning_rate_placeholder = tf.placeholder(tf.float32)
+train_step = tf.train.GradientDescentOptimizer(learning_rate_placeholder).minimize(cross_entropy)
+
 
 # matplotlib visualisation
 allweights = tf.reshape(W, [-1])
@@ -91,6 +115,12 @@ def training_step(i, update_test_data, update_train_data):
     # training on batches of 100 images with 100 labels
     batch_X, batch_Y = mnist.train.next_batch(100)
 
+    # learning rate decay
+    max_learning_rate = 0.003
+    min_learning_rate = 0.0001
+    decay_speed = 2000  # 0.003-0.0001-2000=>0.9826 done in 5000 iterations
+    learning_rate = min_learning_rate + (max_learning_rate - min_learning_rate) * math.exp(float(-i)/decay_speed)
+
     # compute training values for visualisation
     if update_train_data:
         a, c, im, w, b = sess.run([accuracy, cross_entropy, I, allweights, allbiases], feed_dict={X: batch_X, Y_: batch_Y})
@@ -104,10 +134,10 @@ def training_step(i, update_test_data, update_train_data):
         a, c, im = sess.run([accuracy, cross_entropy, It], feed_dict={X: mnist.test.images, Y_: mnist.test.labels})
         datavis.append_test_curves_data(i, a, c)
         datavis.update_image2(im)
-        print(str(i) + ": ********* epoch " + str(i*100//mnist.train.images.shape[0]+1) + " ********* test accuracy:" + str(a) + " test loss: " + str(c))
+        print(str(i) + ": ********* epoch " + str(i*100//mnist.train.images.shape[0]+1) + " ********* test accuracy:" + str(a) + " test loss: " + str(c) + ' learn rate: ' + str(learning_rate))
 
     # the backpropagation training step
-    sess.run(train_step, feed_dict={X: batch_X, Y_: batch_Y})
+    sess.run(train_step, feed_dict={X: batch_X, Y_: batch_Y, learning_rate_placeholder: learning_rate})
 
 
 datavis.animate(training_step, iterations=2000+1, train_data_update_freq=10, test_data_update_freq=50, more_tests_at_start=True)
